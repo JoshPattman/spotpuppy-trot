bebc63af13426c6daac3505d7373d61f533e44f1
Josh Pattman <Josh.pattman@gmail.com>
2021-11-11 16:59:24 +0000

    changed pushup so it only affects legs on floor

--- model_trot.py
+++ model_trot.py
@@ -9,11 +9,14 @@ import math, time
 
 
 # Must extend the base class
-class quadruped (quadruped_base.quadruped):
-    def __init__(self, rotation_sensor=None, servo_controller=None, bone_length=6, body_dims=[10, 10], fall_rotation_limit=0, lay_height=3):
+class quadruped(quadruped_base.quadruped):
+    def __init__(self, rotation_sensor=None, servo_controller=None, bone_length=6, body_dims=[10, 10],
+                 fall_rotation_limit=0, lay_height=3):
         # Initialise the base class
-        quadruped_base.quadruped.__init__(self, rotation_sensor=rotation_sensor,servo_controller=servo_controller,bone_length=bone_length,body_dims=body_dims,fall_rotation_limit=fall_rotation_limit)
-        
+        quadruped_base.quadruped.__init__(self, rotation_sensor=rotation_sensor, servo_controller=servo_controller,
+                                          bone_length=bone_length, body_dims=body_dims,
+                                          fall_rotation_limit=fall_rotation_limit)
+
         # All variables below are custom variables that are specific to this custom quadruped. They are used to control the robot
 
         # Scripts can directly modify these to control the robot
@@ -35,7 +38,6 @@ class quadruped (quadruped_base.quadruped):
 
         # These are the pid controllers for pid pushup
 
-
         # This stores the last time that the robot updated
         self.last_update_time = time.time()
         # This is the timer for the robot, it goes up by self.frequency every second
@@ -44,15 +46,15 @@ class quadruped (quadruped_base.quadruped):
     # This method is a special method that gets called once every update step 
     def _on_update(self):
         current_t = time.time()
-        self.t += (current_t - self.last_update_time)*self.trot_frequency
+        self.t += (current_t - self.last_update_time) * self.trot_frequency
         self.last_update_time = current_t
         self.state_trot()
 
     # The behaviour for the trot state
     def state_trot(self):
         # ta and tb are clocks with values from 0-1 but tb is offset by half a cycle
-        ta = self.t%1.0
-        tb = (self.t+0.5)%1.0
+        ta = self.t % 1.0
+        tb = (self.t + 0.5) % 1.0
         # v_clock contains the foot heights (from 0-1) for ta and tb
         v_clock = [get_foot_height(ta, s=self.trot_air_multiplier), get_foot_height(tb, s=self.trot_air_multiplier)]
         # h_clock contains foot horizontal offsets (from 0-1) for ta and tb
@@ -62,42 +64,56 @@ class quadruped (quadruped_base.quadruped):
         leg_turn_sync = [-1, -1, 1, 1]
         roll_pitch = self.get_roll_pitch()
         push_pids = [self._trot_push_pids[0].update(roll_pitch[1]), self._trot_push_pids[1].update(roll_pitch[0])]
-        r_p_legs = [
-                (push_pids[0]-push_pids[1]),
-                (push_pids[0]+push_pids[1]),
-                (-push_pids[0]-push_pids[1]),
-                (-push_pids[0]+push_pids[1])
-                ]
+        push_legs = [
+            (push_pids[0] - push_pids[1]),
+            (push_pids[0] + push_pids[1]),
+            (-push_pids[0] - push_pids[1]),
+            (-push_pids[0] + push_pids[1])
+        ]
         for l in range(4):
             # Get the position of the foot on the floor under the shoulder when body is at height 8
-            f_pos = self.get_vector_to_robot_center(l, "body")\
-                    + (self.get_dir("global.down") * 8)\
+            f_pos = self.get_vector_to_robot_center(l, "body") \
+                    + (self.get_dir("global.down") * 8) \
                     - self.get_vector_to_robot_center(l, "global")
             # Calculate the vectors for step offset
-            v_off = v_clock[leg_sync[l]] * self.trot_step_height * self.get_dir("global.down") * -1
+            # Vertical
+            v_off = 0
+            if push_legs[l] > 0:
+                v_off = map_range(v_clock[leg_sync[l]], 0, 1, -push_legs[l], self.trot_step_height)\
+                        * self.trot_step_height * -1 * self.get_dir("global.down")
+            else:
+                v_off = map_range(v_clock[leg_sync[l]], 0, 1, -push_legs[l], self.trot_step_height-push_legs) \
+                        * self.trot_step_height * -1 * self.get_dir("global.down")
+            # Forwards
             f_off = h_clock[leg_sync[l]] * self.trot_step_length[0] * self.get_dir("global.forward")
+            # Left
             l_off = h_clock[leg_sync[l]] * self.trot_step_length[1] * self.get_dir("global.left")
+            # Right
             r_off = h_clock[leg_sync[l]] * self.trot_turn * self.get_dir("global.left") * leg_turn_sync[l]
-            pushup_offset = self.get_dir("global.down") * r_p_legs[l]
-            lean_offset = (self.get_dir("global.forward")*self.lean[0]) + (self.get_dir("global.left")*self.lean[1])
-            self.quad_controller.set_leg(l, f_pos + v_off + f_off + l_off + r_off + pushup_offset + lean_offset)
-            #self.quad_controller.set_leg(l, self.get_dir("body.down")*8 +  pushup_offset)
+            # Lean
+            lean_offset = (self.get_dir("global.forward") * self.lean[0]) + (self.get_dir("global.left") * self.lean[1])
+            self.quad_controller.set_leg(l, f_pos + v_off + f_off + l_off + r_off + lean_offset)
 
 
 # t is the timer, s is a value from 0-1 which denotes the length of time that the foot is in the air
 # at s = 1, as soon as one foot is place the other comes up, at s = 0.5, half the time both feet are on the ground
 def get_foot_height(t, s=1):
-    if t < 0.5*s:
-        return math.sin(t*3.14*2/s)
+    if t < 0.5 * s:
+        return math.sin(t * 3.14 * 2 / s)
     return 0
 
 
 def get_foot_horiz(t, s=1):
-    if t < 0.5*s:
-        return from_0to1_to_m1to1(2*t/s)
+    if t < 0.5 * s:
+        return from_0to1_to_m1to1(2 * t / s)
     m = 2 / (s - 2)
-    return from_0to1_to_m1to1(m*t - m)
+    return from_0to1_to_m1to1(m * t - m)
 
 
 def from_0to1_to_m1to1(x):
-    return (x-0.5)*2
+    #return (x - 0.5) * 2
+    return map_range(x, 0, 1, -1, 1)
+
+
+def map_range(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
